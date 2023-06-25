2df4c5400b2e2ecf8142d03d1e9718970ba2aecd
hindlet <79304662+hindlet@users.noreply.github.com>
2023-06-25 10:32:40 +0100

    gui improvements

--- examples/gui.rs
+++ examples/gui.rs
@@ -1,11 +1,9 @@
-use rust_vulkan_graphics::{
-    get_general_graphics_data,
-    create_gui_window, draw_gui_window,
-};
+use rust_vulkan_graphics::*;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::ControlFlow,
 };
+// use std::time::Instant;
 
 fn main() {
 
@@ -13,18 +11,24 @@ fn main() {
     let mut gui = create_gui_window(
         "Example Gui".to_string(),
         vec![("Do nothing".to_string(), false), ("Do something (lie)".to_string(), false)],
+        vec![("Random Slider".to_string(), 0.5, 0.0..=1.0)],
+        vec![("Random int Slider".to_string(), 5, 0..=15)],
         Vec::new(),
         Vec::new(),
+        vec![("Unsigned int setting".to_string(), 5)],
+        vec![("Name Field".to_string(), "Mr Testing".to_string())],
 
         &mut vulkano_windows, window_ids[0], &event_loop
     );
+
+    // let mut last_print_time = Instant::now();
 
     event_loop.run(move |event, _, control_flow| {
         let renderer = vulkano_windows.get_renderer_mut(window_ids[0]).unwrap();
         match event {
             Event::WindowEvent { event, window_id } if window_id == renderer.window().id() => {
                 // Update Egui integration so the UI works!
-                let _pass_events_to_game = !gui.gui.update(&event);
+                let _pass_events_to_game = !update_gui_window(&mut gui, &event);
                 match event {
                     WindowEvent::Resized(_) => {
                         renderer.resize();
@@ -45,13 +49,16 @@ fn main() {
                 // Acquire swapchain future
                 let before_future = renderer.acquire().unwrap();
                 // Render gui
-                let after_future =
-                    gui.gui.draw_on_image(before_future, renderer.swapchain_image_view());
+                let after_future = draw_gui_on_image(&mut gui, before_future, renderer);
                 // Present swapchain
                 renderer.present(after_future, true);
             }
             Event::MainEventsCleared => {
                 renderer.window().request_redraw();
+                // if Instant::now().duration_since(last_print_time).as_secs_f32() > 2.5 {
+                //     last_print_time = Instant::now();
+                //     println!("{:?}", gui);
+                // }
             }
             _ => (),
         }
--- src/gui.rs
+++ src/gui.rs
@@ -1,8 +1,9 @@
 use std::{ops::RangeInclusive, fmt::Debug};
-use egui::{self, Color32, Context, FontFamily, FontDefinitions, FontData, epaint::Shadow, Stroke, CentralPanel, ScrollArea, Checkbox, Slider, Label};
+use egui::{self, Color32, Context, FontFamily, FontDefinitions, FontData, epaint::Shadow, Stroke, CentralPanel, ScrollArea, Checkbox, Slider, Label, TextEdit, DragValue};
 use egui_winit_vulkano::{Gui, GuiConfig};
-use vulkano_util::window::VulkanoWindows;
-use winit::{window::WindowId, event_loop::EventLoop};
+use vulkano::sync::GpuFuture;
+use vulkano_util::{window::VulkanoWindows, renderer::VulkanoWindowRenderer};
+use winit::{window::WindowId, event_loop::EventLoop, event::WindowEvent};
 
 
 pub struct GuiWindowData {
@@ -10,19 +11,38 @@ pub struct GuiWindowData {
     pub checkboxes: Vec<(String, bool)>,
     pub f32_sliders: Vec<(String, f32, RangeInclusive<f32>)>,
     pub i32_sliders: Vec<(String, i32, RangeInclusive<i32>)>,
-    pub gui: Gui
+    pub f32_boxes: Vec<(String, f32)>,
+    pub i32_boxes: Vec<(String, i32)>,
+    pub u32_boxes: Vec<(String, u32)>,
+    pub string_boxes: Vec<(String, String)>,
+
+    has_checkboxes: bool,
+    has_sliders: bool,
+    has_boxes: bool,
+
+    gui: Gui
 }
 
 impl Debug for GuiWindowData {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Gui Window")
-            .field("title", &self.title)
-            .field("checkboxes", &self.checkboxes)
-            .field("float sliders", &self.f32_sliders)
-            .field("int sliders", &self.i32_sliders)
-            .finish()
+        writeln!(f,
+            "Gui Window - {}: \n- Checkboxes: {:?} \n- Float sliders: {:?} \n- Int Sliders: {:?} \n- Float Boxes: {:?} \n- Int Boxes: {:?} \n- Uint Boxes: {:?} \n- String Boxes: {:?}",
+            self.title, self.checkboxes, self.f32_sliders, self.i32_sliders, self.f32_boxes, self.i32_boxes, self.u32_boxes, self.string_boxes
+        )
+        // f.debug_struct("Gui Window")
+        //     .field("title", &self.title)
+        //     .field("checkboxes", &self.checkboxes)
+        //     .field("float sliders", &self.f32_sliders)
+        //     .field("int sliders", &self.i32_sliders)
+        //     .field("float boxes", &self.f32_boxes)
+        //     .field("int boxes", &self.i32_boxes)
+        //     .field("uint boxes", &self.u32_boxes)
+        //     .field("string boxes", &self.string_boxes)
+        //     .finish()
     }
 }
+
+
 
 
 /// this just sets the gui style to my preffered
@@ -68,6 +88,10 @@ pub fn create_gui_window(
     checkboxes: Vec<(String, bool)>,
     f32_sliders: Vec<(String, f32, RangeInclusive<f32>)>,
     i32_sliders: Vec<(String, i32, RangeInclusive<i32>)>,
+    f32_boxes: Vec<(String, f32)>,
+    i32_boxes: Vec<(String, i32)>,
+    u32_boxes: Vec<(String, u32)>,
+    string_boxes: Vec<(String, String)>,
 
     windows: &mut VulkanoWindows,
     window_id: WindowId,
@@ -76,13 +100,41 @@ pub fn create_gui_window(
     let renderer = windows.get_renderer_mut(window_id).unwrap();
     let gui = Gui::new(event_loop, renderer.surface(), renderer.graphics_queue(), GuiConfig::default());
     set_gui_style(&gui.context());
+    let has_checkboxes = !(checkboxes.len() == 0);
+    let has_sliders = !(f32_sliders.len() == 0 && i32_sliders.len() == 0);
+    let has_boxes = !(f32_boxes.len() == 0 && i32_boxes.len() == 0 && u32_boxes.len() == 0 && string_boxes.len() == 0);
+
     GuiWindowData {
         title,
         checkboxes,
         f32_sliders,
         i32_sliders,
+        f32_boxes,
+        i32_boxes,
+        u32_boxes,
+        string_boxes,
+
+        has_checkboxes,
+        has_sliders,
+        has_boxes,
+
         gui
     }
+}
+
+pub fn update_gui_window(
+    window: &mut GuiWindowData,
+    event: &WindowEvent
+) -> bool {
+    window.gui.update(event)
+}
+
+pub fn draw_gui_on_image(
+    window: &mut GuiWindowData,
+    before_future: Box<dyn GpuFuture>,
+    renderer: &mut VulkanoWindowRenderer,
+) -> Box<dyn GpuFuture>{
+    window.gui.draw_on_image(before_future, renderer.swapchain_image_view())
 }
 
 
@@ -100,22 +152,49 @@ pub fn draw_gui_window(
             ScrollArea::vertical().id_source("settings").show(
                 ui,
                 |ui| {
-                    ui.vertical_centered(|ui| {
-                        sized_text(ui, "Checkboxes", 16.0);
-                    });
-                    for checkbox_data in data.checkboxes.iter_mut() {
-                        ui.add(Checkbox::new(&mut checkbox_data.1, &checkbox_data.0));
+                    if data.has_checkboxes {
+                        ui.vertical_centered(|ui| {
+                            sized_text(ui, "Checkboxes", 16.0);
+                        });
+                        for checkbox_data in data.checkboxes.iter_mut() {
+                            ui.add(Checkbox::new(&mut checkbox_data.1, &checkbox_data.0));
+                        }
+                        ui.separator();
                     }
-                    ui.vertical_centered(|ui| {
-                        sized_text(ui, "Sliders", 16.0);
-                    });
-                    for slider_data in data.f32_sliders.iter_mut() {
-                        ui.add(Label::new(slider_data.0.clone()));
-                        ui.add(Slider::new(&mut slider_data.1, slider_data.2.clone()));
+                    if data.has_sliders {
+                        ui.vertical_centered(|ui| {
+                            sized_text(ui, "Sliders", 16.0);
+                        });
+                        for slider_data in data.f32_sliders.iter_mut() {
+                            ui.add(Label::new(slider_data.0.clone()));
+                            ui.add(Slider::new(&mut slider_data.1, slider_data.2.clone()));
+                        }
+                        for slider_data in data.i32_sliders.iter_mut() {
+                            ui.add(Label::new(slider_data.0.clone()));
+                            ui.add(Slider::new(&mut slider_data.1, slider_data.2.clone()));
+                        }
+                        ui.separator();
                     }
-                    for slider_data in data.i32_sliders.iter_mut() {
-                        ui.add(Label::new(slider_data.0.clone()));
-                        ui.add(Slider::new(&mut slider_data.1, slider_data.2.clone()));
+                    if data.has_boxes {
+                        ui.vertical_centered(|ui| {
+                            sized_text(ui, "Data Boxes", 16.0);
+                        });
+                        for f32_data in data.f32_boxes.iter_mut() {
+                            ui.add(Label::new(f32_data.0.clone()));
+                            ui.add(DragValue::new(&mut f32_data.1));
+                        }
+                        for i32_data in data.i32_boxes.iter_mut() {
+                            ui.add(Label::new(i32_data.0.clone()));
+                            ui.add(DragValue::new(&mut i32_data.1));
+                        }
+                        for u32_data in data.u32_boxes.iter_mut() {
+                            ui.add(Label::new(u32_data.0.clone()));
+                            ui.add(DragValue::new(&mut u32_data.1));
+                        }
+                        for string_data in data.string_boxes.iter_mut() {
+                            ui.add(Label::new(string_data.0.clone()));
+                            ui.add(TextEdit::singleline(&mut string_data.1));
+                        }
                     }
                 }
             )
