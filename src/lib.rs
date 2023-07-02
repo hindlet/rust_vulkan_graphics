21e26e15601d394f3444c8897308277efc52410b
hindlet <79304662+hindlet@users.noreply.github.com>
2023-07-02 21:38:02 +0100

    added vulkano context to use statements

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ pub use vulkano_wrapping::*;
 pub use vulkano::buffer::{allocator::*, Subbuffer};
 pub use meshes::*;
 pub use winit::{event::{Event, WindowEvent, ElementState}, event_loop::ControlFlow};
+pub use vulkano_util::context::VulkanoContext;
 
 pub mod vertex_types{
     pub use super::general_graphics::{ColouredVertex, PositionVertex, Normal};
