7c3b05a8e63618acd346ac859295249d58fa8011
hindlet <79304662+hindlet@users.noreply.github.com>
2023-07-02 13:28:43 +0100

    added generic mesh type

--- src/meshes/coloured_mesh.rs
+++ src/meshes/coloured_mesh.rs
@@ -12,9 +12,7 @@ use maths::Vector3;
 /// Data for a mesh in 3D space
 #[derive(Debug, Clone)]
 pub struct ColouredMesh {
-    pub vertices: Vec<PositionVertex>,
-    pub normals: Vec<Normal>,
-    pub indices: Vec<u32>,
+    mesh: Mesh,
     pub colour: [f32; 4],
 }
 
@@ -26,7 +24,7 @@ impl ColouredMesh {
         colour: [f32; 4]
     ) -> Self {
         Self {
-            vertices, normals: Vec::new(), indices, colour
+            mesh: Mesh::new(vertices, indices), colour
         }
     }
 
@@ -35,82 +33,28 @@ impl ColouredMesh {
         &mut self,
         normals: Vec<Normal>,
     ) -> &mut ColouredMesh{
-        self.normals = normals;
+        self.mesh.set_normals(normals);
         self
     }
 
     // recalculates the normals of the given mesh, smooth shaded
     pub fn recalculate_normals(&mut self) -> &mut ColouredMesh{
-        // init normals
-        let mut normals: Vec<Vector3> = Vec::new();
-        for _i in 0..self.vertices.len() {
-            normals.push([0.0, 0.0, 0.0].into())
-        }
-        
-        // create normals
-        for i in (0..self.indices.len()).step_by(3) {
-            let dir_one: Vector3 = {
-                let dir: Vector3 = self.vertices[self.indices[i] as usize].position.into();
-                dir - self.vertices[self.indices[i + 2] as usize].position.into()
-            };
-            let dir_two: Vector3 = {
-                let dir: Vector3 = self.vertices[self.indices[i + 1] as usize].position.into();
-                dir - self.vertices[self.indices[i + 2] as usize].position.into()
-            };
-            let normal = dir_one.cross(dir_two);
-
-            normals[self.indices[i + 0] as usize] += normal;
-            normals[self.indices[i + 1] as usize] += normal;
-            normals[self.indices[i + 2] as usize] += normal;
-        }
-
-        // normalise normals
-        let normals = {
-            let mut final_norms: Vec<Normal> = Vec::new();
-            for normal in normals.iter_mut() {
-                // println!("{:?}, {:?}", normal, normal.normalised());
-                final_norms.push(normal.normalised().into());
-                
-            }
-            final_norms
-        };
-
-        self.set_normals(normals);
+        self.mesh.recalculate_normals();
         self
     }
 
     /// returns a flat shaded version of the mesh called on
     pub fn flat_shaded(&self) -> ColouredMesh {
-        let mut new_verts: Vec<PositionVertex> = Vec::new();
-        let mut new_normals: Vec<Normal> = Vec::new();
-    
-        for i in (0..self.indices.len()).step_by(3) {
-            let v_one: Vector3 = self.vertices[self.indices[i as usize + 0] as usize].position.into();
-            let v_two: Vector3 = self.vertices[self.indices[i as usize + 1] as usize].position.into();
-            let v_thr: Vector3 = self.vertices[self.indices[i as usize + 2] as usize].position.into();
-    
-            let normal = (v_one - v_thr).cross(v_two - v_thr).normalised();
-    
-            new_verts.push(PositionVertex::from(v_one));
-            new_verts.push(PositionVertex::from(v_two));
-            new_verts.push(PositionVertex::from(v_thr));
-            new_normals.push(Normal::from(normal));
-            new_normals.push(Normal::from(normal));
-            new_normals.push(Normal::from(normal));
-        }
-    
-        let indices = (0..(new_verts.len()) as u32).collect_vec();
-        ColouredMesh::new(new_verts, indices, self.colour).set_normals(new_normals).clone()
+        let new_mesh = self.mesh.flat_shaded();
+
+        ColouredMesh {mesh: new_mesh, colour: self.colour}
     }
 
     /// sets the current mesh to be flat shaded
     /// 
     /// NOT CURRENTLY REVERSIBLE
     pub fn flat_shade(&mut self) {
-        let new = self.flat_shaded();
-        self.vertices = new.vertices;
-        self.normals = new.normals;
-        self.indices = new.indices;
+        self.mesh.flat_shade();
     }
 
     /// flat shades the components of a Mesh without ever needing a Mesh
@@ -141,14 +85,10 @@ impl ColouredMesh {
 
 
     pub fn components(&self) -> (Vec<PositionVertex>, Vec<Normal>, Vec<u32>){
-        (self.vertices.clone(), self.normals.clone(), self.indices.clone())
+        self.mesh.components()
     }
 
     pub fn get_buffers(&self, context: VulkanoContext) -> (Subbuffer<[PositionVertex]>, Subbuffer<[Normal]>, Subbuffer<[u32]>) {
-        (
-            create_graphics_shader_data_buffer(self.vertices.clone(), &context, BufferType::Vertex),
-            create_graphics_shader_data_buffer(self.normals.clone(), &context, BufferType::Normal),
-            create_graphics_shader_data_buffer(self.indices.clone(), &context, BufferType::Index),
-        )
+        self.mesh.get_buffers(context)
     }
 }
--- src/meshes/mod.rs
+++ src/meshes/mod.rs
@@ -1,4 +1,6 @@
 mod coloured_mesh;
+mod mesh;
 use super::{vertex_types::*, create_graphics_shader_data_buffer, BufferType};
 
 pub use coloured_mesh::ColouredMesh;
+pub use mesh::Mesh;
