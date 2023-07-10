cb222b94a2e980c6711ddcb6cac81e9b7227fc20
hindlet <79304662+hindlet@users.noreply.github.com>
2023-07-10 20:38:59 +0100

    updated use statements

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ pub use vulkano_shaders::shader;
 pub use vulkano::pipeline::ComputePipeline;
 pub use general_compute::*;
 pub use vulkano::descriptor_set::{allocator::StandardDescriptorSetAllocator, WriteDescriptorSet};
-pub use vulkano::command_buffer::allocator::CommandBufferAllocator;
+pub use vulkano::command_buffer::allocator::StandardCommandBufferAllocator;
 
 pub mod vertex_types{
     pub use super::general_graphics::{ColouredVertex, PositionVertex, Normal};
