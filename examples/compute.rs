f9961d9d98b6d0da0ec58de32d7d397fb615e0ed
hindlet <79304662+hindlet@users.noreply.github.com>
2023-07-10 13:36:44 +0100

    added push constant use options

--- examples/compute.rs
+++ examples/compute.rs
@@ -3,6 +3,7 @@ use rust_vulkan_graphics::*;
 fn main() {
     let (context, command_allocator, descript_allocator) = get_general_compute_data();
     let data_buffer = create_shader_data_buffer(0..65536u32, &context, BufferType::Storage);
+    const MULTIPLIER: u32 = 4;
 
     let shader = cs::load(context.device().clone()).unwrap();
 
@@ -10,11 +11,15 @@ fn main() {
 
     let descriptor_set = get_descriptor_set(&pipeline, &descript_allocator, 0, [WriteDescriptorSet::buffer(0, data_buffer.clone())]);
 
-    run_compute_operation(&context, &command_allocator, &pipeline, [65536, 1, 1], [64, 1, 1], descriptor_set, None);
+    let push_constants = cs::PushConstantData {
+        multiple: MULTIPLIER
+    };
+
+    run_compute_operation_push_constants(&context, &command_allocator, &pipeline, [65536, 1, 1], [64, 1, 1], descriptor_set, push_constants, None);
 
     let content = data_buffer.read().unwrap();
     for (n, val) in content.iter().enumerate() {
-        assert_eq!(*val, n as u32 * 12);
+        assert_eq!(*val, n as u32 * MULTIPLIER);
     }
 
     println!("Everything succeeded!");
@@ -28,13 +33,17 @@ mod cs {
 
             layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
 
+            layout(push_constant) uniform PushConstantData {
+                uint multiple;
+            } pc;
+
             layout(set = 0, binding = 0) buffer Data {
                 uint data[];
             } buf;
 
             void main() {
                 uint idx = gl_GlobalInvocationID.x;
-                buf.data[idx] *= 12;
+                buf.data[idx] *= pc.multiple;
             }
         ",
     }
--- src/general_compute.rs
+++ src/general_compute.rs
@@ -4,7 +4,7 @@ use vulkano::{
     command_buffer::{allocator::StandardCommandBufferAllocator, AutoCommandBufferBuilder, CommandBufferUsage},
     pipeline::{ComputePipeline, Pipeline, PipelineBindPoint},
     shader::ShaderModule,
-    sync::{self, GpuFuture},
+    sync::{self, GpuFuture}, buffer::BufferContents,
 };
 use vulkano_util::context::{VulkanoContext, VulkanoConfig};
 
@@ -63,7 +63,8 @@ pub fn run_compute_operation<S>(
     descriptor_sets: S,
     timeout: Option<Duration>
 )
-where S: DescriptorSetsCollection
+where
+    S: DescriptorSetsCollection,
 {
     let mut builder = AutoCommandBufferBuilder::primary(
         command_allocator,
@@ -73,6 +74,49 @@ where S: DescriptorSetsCollection
 
     builder
         .bind_pipeline_compute(pipeline.clone())
+        .bind_descriptor_sets(
+            PipelineBindPoint::Compute,
+            pipeline.layout().clone(),
+            0,
+            descriptor_sets
+        )
+        .dispatch([num_to_process[0] / work_group_size[0], num_to_process[1] / work_group_size[1], num_to_process[2] / work_group_size[2]])
+        .unwrap();
+
+    let command_buffer = builder.build().unwrap();
+
+    let future = sync::now(context.device().clone())
+        .then_execute(context.compute_queue().clone(), command_buffer)
+        .unwrap()
+        .then_signal_fence_and_flush()
+        .unwrap();
+
+    future.wait(timeout).unwrap();
+}
+
+pub fn run_compute_operation_push_constants<S, T>(
+    context: &VulkanoContext,
+    command_allocator: &Arc<StandardCommandBufferAllocator>,
+    pipeline: &Arc<ComputePipeline>,
+    num_to_process: [u32; 3],
+    work_group_size: [u32; 3],
+    descriptor_sets: S,
+    push_constants: T,
+    timeout: Option<Duration>
+)
+where
+    S: DescriptorSetsCollection,
+    T: BufferContents,
+{
+    let mut builder = AutoCommandBufferBuilder::primary(
+        command_allocator,
+        context.compute_queue().queue_family_index(),
+        CommandBufferUsage::OneTimeSubmit
+    ).unwrap();
+
+    builder
+        .bind_pipeline_compute(pipeline.clone())
+        .push_constants(pipeline.layout().clone(), 0, push_constants)
         .bind_descriptor_sets(
             PipelineBindPoint::Compute,
             pipeline.layout().clone(),
